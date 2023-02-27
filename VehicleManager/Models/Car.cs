2908611e72af5257559e8b7752733e90cc0e5e52
Niklas Gustavsson <niklas.gustavsson86@gmail.com>
2023-02-27 09:56:23 +0100

    Added class Car to Models folder

---
+++ VehicleManager/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleManager.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public int VehicleCategoryId { get; set; }
+        public string Brand { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string ImgUrl { get; set; } = "https://upload.wikimedia.org/wikipedia/commons/1/1d/Opel_Kadett_C_Coupe.jpg";
+        public DateTime RentalStart { get; set; }
+        public DateTime RentalEnd { get; set; }
+    }
+}
