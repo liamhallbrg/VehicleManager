810851e7cdfe19700595668af3fea719d05271de
Liam Hallberg <liam.hallberg@yaelev.se>
2023-02-27 10:32:34 +0100

    Added Description in vehiclecategory

--- VehicleManager/Models/VehicleCategory.cs
+++ VehicleManager/Models/VehicleCategory.cs
@@ -4,6 +4,7 @@
     {
         public int VehicleCategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         virtual public List<Car>? Cars { get; set; }
     }
 }
