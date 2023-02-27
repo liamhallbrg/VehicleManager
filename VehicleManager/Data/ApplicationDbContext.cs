357c432ebfdd67b6682ae614bbb04c94b9018753
Niklas Gustavsson <niklas.gustavsson86@gmail.com>
2023-02-27 10:24:27 +0100

    Added Data folder

---
+++ VehicleManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace VehicleManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<VehicleManager.Models.Car> Cars { get; set; } = default!;
+    }
+}
