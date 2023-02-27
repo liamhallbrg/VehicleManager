d0ec6632ca49624319755a9fa433e4922865fff5
Erik Sundkvist <egsundkvist@hotmail.com>
2023-02-27 11:38:10 +0100

    Added Customer class

---
+++ VehicleManager/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace VehicleManager.Models
+{
+	public class Customer
+	{
+		public int CustomerId { get; set; }
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public string FullName => $"{FirstName} {LastName}";
+		public string Email { get; set; } = string.Empty;
+		public string Address { get; set; } = string.Empty;
+		public string City { get; set; } = string.Empty;
+        public int DriverLicenceNr { get; set; }
+
+    }
+}
--- VehicleManager/Models/Rental.cs
+++ VehicleManager/Models/Rental.cs
@@ -4,7 +4,7 @@
 	{
         public int RentalId { get; set; }
         public int CarId { get; set; }
-        public int UserId { get; set; }
+        public int CustomerId { get; set; }
         public DateTime PickUpDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public DateTime BookingMade { get; set; } = DateTime.Now;
