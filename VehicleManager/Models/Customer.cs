4fcd90a651e36c86f23ec6db74369123696f825f
Erik Sundkvist <egsundkvist@hotmail.com>
2023-02-27 18:42:26 +0100

    Added dataannotations to Customer

--- VehicleManager/Models/Customer.cs
+++ VehicleManager/Models/Customer.cs
@@ -1,15 +1,26 @@
-﻿namespace VehicleManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehicleManager.Models
 {
 	public class Customer
 	{
 		public int CustomerId { get; set; }
+		[Required]
+		[MaxLength(40)]
 		public string FirstName { get; set; } = string.Empty;
+		[Required]
+		[MaxLength(40)]
 		public string LastName { get; set; } = string.Empty;
 		public string FullName => $"{FirstName} {LastName}";
+		[Required]
 		public string Email { get; set; } = string.Empty;
+		[Required]
 		public string Address { get; set; } = string.Empty;
+		[Required]
 		public string City { get; set; } = string.Empty;
-        public int DriverLicenceNr { get; set; }
+		[Required]
+		[StringLength(9)]
+		public int DriverLicenceNr { get; set; }
 
     }
 }
