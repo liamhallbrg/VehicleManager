3faa7e59f713a76cea0248c388168dfca12df383
Liam Hallberg <liam.hallberg@yaelev.se>
2023-02-28 09:16:23 +0100

    .

--- VehicleManager/Models/VehicleCategory.cs
+++ VehicleManager/Models/VehicleCategory.cs
@@ -5,9 +5,11 @@ namespace VehicleManager.Models
     public class VehicleCategory
     {
         public int VehicleCategoryId { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         [MaxLength(120)]
         public string Description { get; set; } = string.Empty;
+        [Required]
         public double PricePerDay { get; set; }
         virtual public List<Car>? Cars { get; set; }
     }
