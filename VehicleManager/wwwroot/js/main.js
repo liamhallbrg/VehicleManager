596b783c81cae34daf31180d0cc32566071ec6f6
Molander <molander.johnny@gmail.com>
2023-03-02 13:51:05 +0100

    Added Profile Link

--- VehicleManager/wwwroot/js/main.js
+++ VehicleManager/wwwroot/js/main.js
@@ -108,29 +108,30 @@
 	};
 	carousel();
 
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
-	});
+	//$('nav .dropdown').hover(function(){
+	//	var $this = $(this);
+	//	// 	 timer;
+	//	// clearTimeout(timer);
+	//	$this.addClass('show');
+	//	console.log('show');
+	//	$this.find('> a').attr('aria-expanded', true);
+	//	// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+	//	$this.find('.dropdown-menu').addClass('show');
+	//}, function(){
+	//	var $this = $(this);
+	//		// timer;
+	//	// timer = setTimeout(function(){
+	//		$this.removeClass('show');
+	//		$this.find('> a').attr('aria-expanded', false);
+	//		// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+	//		$this.find('.dropdown-menu').removeClass('show');
+	//	// }, 100);
+	//});
 
 
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
-	});
+	//$('#dropdown04').on('show.bs.dropdown', function () {
+	//  console.log('show');
+	//});
 
 	// scroll
 	var scrollWindow = function() {
