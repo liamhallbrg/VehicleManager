03988d0e11633e81fbd76dc61f0b6dbc9df243fb
Liam Hallberg <liam.hallberg@yaelev.se>
2023-03-03 11:19:43 +0100

    added more seed data

--- VehicleManager/Data/ApplicationDbContext.cs
+++ VehicleManager/Data/ApplicationDbContext.cs
@@ -15,24 +15,111 @@ namespace VehicleManager.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<VehicleCategory>().HasData(
-                new VehicleCategory { VehicleCategoryId = 1, Name = "Economy", PricePerDay = 49 },
-                new VehicleCategory { VehicleCategoryId = 2, Name = "Compact", PricePerDay = 59 },
-                new VehicleCategory { VehicleCategoryId = 3, Name = "Midsize", PricePerDay = 69 },
-                new VehicleCategory { VehicleCategoryId = 4, Name = "Full-size", PricePerDay = 79 }
-            );
-
+                new VehicleCategory
+                {
+                    VehicleCategoryId = 1,
+                    Name = "SUV",
+                    Description = "desc",
+                    PricePerDay = 5000,
+                },
+                new VehicleCategory
+                {
+                    VehicleCategoryId = 2,
+                    Name = "Hatchback",
+                    Description = "desc",
+                    PricePerDay = 5000,
+                },
+                new VehicleCategory
+                {
+                    VehicleCategoryId = 3,
+                    Name = "Sedan",
+                    Description = "desc",
+                    PricePerDay = 5000,
+                },
+                new VehicleCategory
+                {
+                    VehicleCategoryId = 4,
+                    Name = "Sport",
+                    Description = "desc",
+                    PricePerDay = 5000,
+                },
+                new VehicleCategory
+                {
+                    VehicleCategoryId = 5,
+                    Name = "Pickup Truck",
+                    Description = "desc",
+                    PricePerDay = 5000,
+                },
+                new VehicleCategory
+                {
+                    VehicleCategoryId = 6,
+                    Name = "Minivan",
+                    Description = "desc",
+                    PricePerDay = 5000,
+                }
+                );
             modelBuilder.Entity<Car>().HasData(
-                new Car { CarId = 1, VehicleCategoryId = 1, Brand = "Toyota", Description = "Economy car", PlateNumber = "ABC123" },
-                new Car { CarId = 2, VehicleCategoryId = 2, Brand = "Ford", Description = "Compact car", PlateNumber = "DEF456" },
-                new Car { CarId = 3, VehicleCategoryId = 3, Brand = "Honda", Description = "Midsize car", PlateNumber = "GHI789" },
-                new Car { CarId = 4, VehicleCategoryId = 4, Brand = "Chevrolet", Description = "Full-size car", PlateNumber = "JKL012" }
-            );
+                new Car
+                {
+                    CarId = 1,
+                    VehicleCategoryId = 1,
+                    Brand = "Kia",
+                    Description = "Sorento",
+                    ImgUrl = "https://www.kia.com/content/dam/kwcms/gt/en/images/discover-kia/voice-search/parts-80-1.jpg",
+                    PlateNumber = "abc123"
+                },
+                new Car
+                {
+                    CarId = 2,
+                    VehicleCategoryId = 2,
+                    Brand = "Kia",
+                    Description = "Rio",
+                    ImgUrl = "https://www.kia.com/content/dam/kwcms/gt/en/images/discover-kia/voice-search/parts-80-2.jpg",
+                    PlateNumber = "abc123"
+                },
+                new Car
+                {
+                    CarId = 3,
+                    VehicleCategoryId = 3,
+                    Brand = "Kia",
+                    Description = "Cadenza",
+                    ImgUrl = "https://www.kia.com/content/dam/kwcms/gt/en/images/discover-kia/voice-search/parts-80-5.jpg",
+                    PlateNumber = "abc123"
+                },
+                new Car
+                {
+                    CarId = 4,
+                    VehicleCategoryId = 4,
+                    Brand = "Kia",
+                    Description = "Stinger",
+                    ImgUrl = "https://www.kia.com/content/dam/kwcms/gt/en/images/discover-kia/voice-search/parts-80-6.jpg",
+                    PlateNumber = "abc123"
+                },
+                new Car
+                {
+                    CarId = 5,
+                    VehicleCategoryId = 5,
+                    Brand = "Kia",
+                    Description = "Telluride Pickup Truck",
+                    ImgUrl = "https://www.kia.com/content/dam/kwcms/gt/en/images/discover-kia/voice-search/parts-80-10.jpg",
+                    PlateNumber = "abc123"
+                },
+                new Car
+                {
+                    CarId = 6,
+                    VehicleCategoryId = 6,
+                    Brand = "Kia",
+                    Description = "Sedona",
+                    ImgUrl = "https://www.kia.com/content/dam/kwcms/gt/en/images/discover-kia/voice-search/parts-80-8.jpg",
+                    PlateNumber = "abc123"
+                }
+                );
 
             modelBuilder.Entity<Customer>().HasData(
                 new Customer { CustomerId = 1, FirstName = "John", LastName = "Doe", Email = "johndoe@gmail.com", Address = "123 Main St", City = "Anytown", DriverLicenceNr = 123456789 },
                 new Customer { CustomerId = 2, FirstName = "Jane", LastName = "Doe", Email = "janedoe@gmail.com", Address = "456 Maple Ave", City = "Othertown", DriverLicenceNr = 987654321 },
                 new Customer { CustomerId = 3, FirstName = "Bob", LastName = "Smith", Email = "bobsmith@gmail.com", Address = "789 Oak St", City = "Somewhere", DriverLicenceNr = 456123789 }
-            );
+);
 
             modelBuilder.Entity<Rental>().HasData(
                 new Rental { RentalId = 1, CarId = 1, CustomerId = 1, PickUpDate = DateTime.Now.AddDays(1), ReturnDate = DateTime.Now.AddDays(5), TotalPrice = 245 },
