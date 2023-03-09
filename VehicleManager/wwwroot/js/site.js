c300d64e660f17cbbad5ffac860ee6aac66b90de
Liam Hallberg <liam.hallberg@yaelev.se>
2023-03-09 15:58:19 +0100

    added navbar focus to current page

--- VehicleManager/wwwroot/js/site.js
+++ VehicleManager/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(function () {
+    if (location.pathname.split("/")[1].length == 0)
+        $("#nav-home").addClass("active")
+    else
+        $('nav li a.nav-highlight[href="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+});
